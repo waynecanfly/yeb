24538ef5ba933047e5faceebfd6e0c8da9c719fe
bruce yang <bruce.yang@anker.com>
2021-10-10 22:22:00 +0800

    新增数据库连接，新增配置文件

---
+++ common/database.go
@@ -0,0 +1,41 @@
+package common
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"yeb/model"
+)
+
+var DB *gorm.DB
+
+func InitDB() *gorm.DB {
+	//driverName := viper.GetString("datasource.driverName")
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
+
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset)
+
+	db, err := gorm.Open(mysql.Open(args), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database, err" + err.Error())
+	}
+
+	dbErr := db.AutoMigrate(&model.User{})
+	if dbErr != nil {
+		return nil
+	}
+	DB = db
+	return db
+}
